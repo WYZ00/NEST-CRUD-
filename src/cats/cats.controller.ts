8e522f8cccab350c20eb45ad8bd4ef9796b6b808
WYZ00 <thanthtettin@gmail.com>
2023-06-09 14:02:21 +0630

    Function add

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -7,6 +7,9 @@ import {
   Post,
   Put,
   Query,
+  NotFoundException,
+  ParseIntPipe,
+  ValidationPipe,
 } from '@nestjs/common';
 import { CreateCatDto } from './dto/create-cat.dto';
 import { UpdateCatDto } from './dto/update-cat.dto';
@@ -22,24 +25,35 @@ export class CatsController {
     //const service = new CatsService();
     return this.catsService.getCats(color);
   }
+
   //GET /cats/:id ---> {...}
   @Get(':id')
-  getOneCat(@Param('id') id: string) {
-    return this.catsService.getCat(+id);
+  getOneCat(@Param('id', ParseIntPipe) id: number) {
+    try {
+      return this.catsService.getCat(id);
+    } catch (err) {
+      throw new NotFoundException();
+    }
   }
+
   //POST /cats
   @Post()
-  createCat(@Body() createCatDto: CreateCatDto) {
+  createCat(@Body(new ValidationPipe()) createCatDto: CreateCatDto) {
     return this.catsService.createCat(createCatDto);
   }
+
   //PUT /cats/:id ---> {...}
   @Put(':id')
-  updateCat(@Param('id') id: string, @Body() updateCatDto: UpdateCatDto) {
-    return this.catsService.updateCat(+id, updateCatDto);
+  updateCat(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateCatDto: UpdateCatDto,
+  ) {
+    return this.catsService.updateCat(id, updateCatDto);
   }
+
   //DELETE /cats/:id
   @Delete(':id')
-  removeCat(@Param('id') id: string) {
-    return this.catsService.removeCat(+id);
+  removeCat(@Param('id', ParseIntPipe) id: number) {
+    return this.catsService.removeCat(id);
   }
 }
--- src/cats/dto/create-cat.dto.ts
+++ src/cats/dto/create-cat.dto.ts
@@ -1,4 +1,8 @@
+import { IsEnum, MinLength } from "class-validator";
+
 export class CreateCatDto {
+  @MinLength(3)
   name: string;
+  @IsEnum(['brown','white'],{message:"Use correct colors!"})
   color: 'brown' | 'white';
 }
